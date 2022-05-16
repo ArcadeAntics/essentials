0b47c587429782e4a16ecd10ff01faf9642544a7
ArcadeAntics <akwsimmo@gmail.com>
2022-05-16 01:33:57 -0700

    rephrasing of packageStartupMessage

--- R/hooks-for-namespace-events.R
+++ R/hooks-for-namespace-events.R
@@ -5,7 +5,7 @@
     nm <- "R_ESSENTIALS_SUPPRESSPACKAGESTARTUPMESSAGES"
     if (Sys.getenv(nm, "FALSE")) {
     } else if (getOption(nm, FALSE)) {
-    } else packageStartupMessage(paste(strwrap(sprintf("the text formatting for essentials::ArgumentParser was changed with the addition of essentials::f.str\n\nplease change variables from %%(DEFAULT) to %%(DEFAULT)s or %%(METAVARIABLE) to %%(METAVARIABLE)s or any similar such uses\n\nsee ?essentials::f.str for details and examples\n\nturn this message permanently off by setting environment variable or R option \"%s\" to TRUE",
+    } else packageStartupMessage(paste(strwrap(sprintf("the string formatting/interpolation for essentials::ArgumentParser was changed with the addition of essentials::f.str\n\nplease change variables from %%(DEFAULT) to %%(DEFAULT)s or %%(METAVARIABLE) to %%(METAVARIABLE)s or any similar such uses\n\nsee ?essentials::f.str for details and examples\n\nturn this message permanently off by setting environment variable or R option \"%s\" to TRUE",
         nm)), collapse = "\n"))
     if (is.na(otoplevel <<- Sys.getenv("R_ESSENTIALS_TOP_LEVEL", NA)))
         Sys.setenv(R_ESSENTIALS_TOP_LEVEL = TRUE)

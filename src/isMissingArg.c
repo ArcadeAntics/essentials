eb5ddc1ce66eacf9353e5dc550f92f5aa47348cf
ArcadeAntics <akwsimmo@gmail.com>
2022-05-16 01:02:26 -0700

    isMissingArg now works recursively

--- src/isMissingArg.c
+++ src/isMissingArg.c
@@ -2,6 +2,44 @@
 #include <Rinternals.h>
 
 
+SEXP isMissingArg(SEXP x, SEXP rho)
+{
+    if (TYPEOF(x) != SYMSXP)
+        error("invalid 'x'");
+    if (TYPEOF(rho) != ENVSXP)
+        error("invalid 'rho'");
+
+
+    SEXP value;
+
+
+    for (; rho != R_EmptyEnv; rho = ENCLOS(rho)) {
+        value = findVarInFrame3(rho, x, TRUE);
+        if (value != R_UnboundValue) {
+            if (TYPEOF(value) == PROMSXP) {
+                if (TYPEOF(PREXPR(value)) != SYMSXP) {
+                    value = PROTECT(eval(value, rho));
+                    defineVar(x, value, rho);
+                    value = ScalarLogical(value == R_MissingArg);
+                    UNPROTECT(1);
+                    return value;
+                }
+                else {
+                    x = PROTECT(PREXPR(value));
+                    rho = PROTECT(PRENV(value));
+                    value = isMissingArg(x, rho);
+                    UNPROTECT(2);
+                    return value;
+                }
+            }
+            else return ScalarLogical(value == R_MissingArg);
+        }
+    }
+    error("object '%s' not found", CHAR(PRINTNAME(x)));
+    return R_NilValue;
+}
+
+
 SEXP do_isMissingArg(SEXP x, SEXP rho)
 {
     SEXP value;
@@ -13,24 +51,9 @@ SEXP do_isMissingArg(SEXP x, SEXP rho)
 
     if (TYPEOF(x) != SYMSXP) {
         value = PROTECT(eval(x, rho));
-        value = PROTECT(ScalarLogical(value == R_MissingArg));
-        UNPROTECT(2);
+        value = ScalarLogical(value == R_MissingArg);
+        UNPROTECT(1);
         return value;
     }
-
-
-    for (; rho != R_EmptyEnv; rho = ENCLOS(rho)) {
-        value = findVarInFrame3(rho, x, TRUE);
-        if (value != R_UnboundValue) {
-            if (TYPEOF(value) == PROMSXP) {
-                value = PROTECT(eval(value, rho));
-                defineVar(x, value, rho);
-                value = ScalarLogical(value == R_MissingArg);
-                UNPROTECT(1);
-                return value;
-            }
-            return ScalarLogical(value == R_MissingArg);
-        }
-    }
-    error("object '%s' not found", CHAR(PRINTNAME(x)));
+    return isMissingArg(x, rho);
 }

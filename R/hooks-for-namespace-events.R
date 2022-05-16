392dc33dfad519820dc21c6e4d9c5473787cd07f
ArcadeAntics <akwsimmo@gmail.com>
2022-05-16 01:31:06 -0700

    added packageStartupMessage about changes to ArgumentParser text formatting/interpolating

--- R/hooks-for-namespace-events.R
+++ R/hooks-for-namespace-events.R
@@ -2,6 +2,11 @@
 {
     # cat("libname = ", sQuote(libname), "\n", sep = "")
     # cat("pkgname = ", sQuote(pkgname), "\n", sep = "")
+    nm <- "R_ESSENTIALS_SUPPRESSPACKAGESTARTUPMESSAGES"
+    if (Sys.getenv(nm, "FALSE")) {
+    } else if (getOption(nm, FALSE)) {
+    } else packageStartupMessage(paste(strwrap(sprintf("the text formatting for essentials::ArgumentParser was changed with the addition of essentials::f.str\n\nplease change variables from %%(DEFAULT) to %%(DEFAULT)s or %%(METAVARIABLE) to %%(METAVARIABLE)s or any similar such uses\n\nsee ?essentials::f.str for details and examples\n\nturn this message permanently off by setting environment variable or R option \"%s\" to TRUE",
+        nm)), collapse = "\n"))
     if (is.na(otoplevel <<- Sys.getenv("R_ESSENTIALS_TOP_LEVEL", NA)))
         Sys.setenv(R_ESSENTIALS_TOP_LEVEL = TRUE)
     else if (otoplevel)

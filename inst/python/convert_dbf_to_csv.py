494bbd6a82fc885da126de6806ecf7e09f992ba7
Andrew Simmons <nsnjsnd@gmail.com>
2024-03-19 16:08:12 -0400

    fix typo

--- inst/python/convert_dbf_to_csv.py
+++ inst/python/convert_dbf_to_csv.py
@@ -8,6 +8,7 @@ def convert_dbf_to_csv(input_dir = None, output_dir = None, names = None):
     if input_dir  is None: input_dir  = '.'
     if output_dir is None: output_dir = '.'
     if names      is None: names = []
+    if isinstance(names, str): names = [names]
     for name in names:
         input_path = os.path.join(input_dir, name)
         output_path = os.path.join(output_dir, name[:-3] + 'csv')
@@ -36,13 +37,13 @@ if __name__ == '__main__':
             if input_dir == '--output':
                 input_dir = output_dir
             elif input_dir.startswith(output_slash):
-                input_dir = output_dir = input_dir[8:]
+                input_dir = output_dir + input_dir[8:]
         elif argv.startswith('--output='):
             output_dir = argv[9:]
             if output_dir == '--input':
                 output_dir = input_dir
             elif output_dir.startswith(input_slash):
-                output_dir = input_dir = output_dir[7:]
+                output_dir = input_dir + output_dir[7:]
         else:
             names += argv.split(os.pathsep)
             if names and names[-1] == '':
